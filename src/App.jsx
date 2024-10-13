bee287e47dc36985ddbfc7b903b5d95168885c28
Lingeshwaransenthilkumar <lingeshwaransenthilkumar0279@gmail.com>
2024-10-13 20:45:56 +0530

    code changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Home from "./pages/home/home";
 import { Routes,Route } from "react-router-dom";
-import Login from "./pages/login/login";
+import Login from "./pages/Login/login";
 import Player from "./pages/player/player";
 import HomePlayer from "./pages/player/home_player";
 function App(){
